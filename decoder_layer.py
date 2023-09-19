e1cfbd5df406c251e5076eae93d82a224d3df7f4
ThomasRochais <thb@sas.upenn.edu>
2023-09-19 09:00:59 -0400

    WIP: building the overall infrastructure

---
+++ decoder_layer.py
@@ -0,0 +1,23 @@
+        
+class DecoderLayer():
+
+    def __init__(self,):
+
+        self.layer_width = 2
+        self.num_layers = 2
+
+
+
+    def forward(self, z):
+        # Decoder $\land$
+
+
+        # Decoder Bernoulli-Categorical
+
+        # Decoder Attention and Word $\pi$
+
+
+        # Decoder Categorical-Bernoulli
+
+
+        # Decoder Open Closed Universe
