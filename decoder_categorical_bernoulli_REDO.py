fixed normalization

decoder_categorical_bernoulli_REDO.py
@@ -25,6 +25,7 @@ class DecoderCategoricalBernoulli(nn.Module):
 
         # we need to convert all of these to bernoullis
         # left:
+        
         u[1][0][0] = v[0][0]
         u[0][0][0] = v[1][0] # for the tails state take the value that v[0][0] is normalized against which is v[1][0]
 
@@ -38,9 +39,7 @@ class DecoderCategoricalBernoulli(nn.Module):
         u[1][1][1] = v[1][1] # the last two indices on the left hand side are equal to the indices on the right hand side
         u[0][1][1] = v[0][1]
 
-        # FIXME: this will not normalize each of the heads/tails signals separately
-        u[:,:,0] = torch.normalize(u[:,:,0], p=1, dim=0)
-        u[:,:,1] = torch.normalize(u[:,:,1], p=1, dim=0)
-        # what we should do instead is:
+        u = torch.normalize(u, p=1, dim=0)
 
         return u
+    

encoder_universe.py
@@ -132,12 +132,7 @@ class EncoderUniverse(nn.Module):
 
         '''
 
-        
-
-
-        # FIXME:
-        u[:,:,0] = torch.normalize(u[:,:,0], p=1, dim=0)
-        u[:,:,1] = torch.normalize(u[:,:,1], p=1, dim=0)
+        u = torch.normalize(u, p=1, dim=0)
         return u
 
         # encoder_bernoulli_categorical.py