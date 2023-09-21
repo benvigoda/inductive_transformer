fd7b94cfdce9ed09889fd2b836f8ee27bba2d3ec
ThomasRochais <thb@sas.upenn.edu>
2023-09-20 20:34:12 -0400

    WIP. Need to redo some math that was in decoder_categorical_bernoulli_x.py but got deleted

---
+++ decoder_universe.py
@@ -0,0 +1,23 @@
+import torch  # type: ignore
+from torch import nn  # type: ignore
+
+
+class EncoderUniverse(nn.Module):
+
+    def __init__(self, hyperparams, active_layer: int):
+        super(EncoderUniverse, self).__init__()
+        self.hyperparams = hyperparams
+        self.active_layer = active_layer
+
+    def forward(self, u):
+
+        # left
+        z[0][0] = u[0][0][0] * u[0][0][1] # all the others
+        z[1][0] = u[1][0][0] * u[1][0][1] + u[1][0][0] * u[0][0][1] + u[0][0][0] * u[1][0][1]
+        # right
+        z[0][1] = u[0][1][0] * u[0][1][1]
+        z[1][1] = u[1][1][0] * u[1][1][1] + u[1][1][0] * u[0][1][1] + u[0][1][0] * u[0][1][1]
+
+        z = torch.normalize(z, p=1, dime=0)
+
+        return z
