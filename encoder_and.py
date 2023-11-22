f0b99c540ed404ac7308d3fabebcbe5e4180e63a
ThomasRochais <thb@sas.upenn.edu>
2023-11-22 16:53:30 -0500

    generalized encoder_and to take any number of layer_width

--- encoder_and.py
+++ encoder_and.py
@@ -18,13 +18,9 @@ class EncoderAnd(nn.Module):
         self.x = x
         self.y = y
 
-        z = torch.empty((2, 2))  # size = (2, layer_width)
-        # left
-        z[1][0] = x[1][0]*y[1][0]  # 1 * 0.5 = 0.5
-        z[0][0] = x[0][0]*y[1][0] + x[1][0]*y[0][0] + x[0][0]*y[0][0]  # 0 + 1 * 0.5 + 0 = 0.5
-
-        z[1][1] = x[1][1]*y[1][1]  # 0 * 0.5 = 0
-        z[0][1] = x[0][1]*y[1][1] + x[1][1]*y[0][1] + x[0][1]*y[0][1]  # 1 * 0.5 + 0 + 0 = 0
+        z = torch.empty((2, self.hyperparams.layer_width))
+        z[1] = x[1] * y[1]
+        z[0] = x[0] * y[1] + x[1] * y[0] + x[0] * y[0]
 
         # z = nn.functional.normalize(z, p=1, dim=0)
         z = custom_normalize(z, dim=0)
