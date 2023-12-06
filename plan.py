236a221a0e5d86509ec5e2e71415bb44f04de506
Erik Strand <erik.strand@cba.mit.edu>
2023-12-06 13:49:28 -0500

    Update notes in plan.py

--- plan.py
+++ plan.py
@@ -4,14 +4,14 @@ Generalization to more than 2 layer_width:
 2. encoder-attention: Done (was already good)
 3. encoder-token: Done (was already good)
 4. encoder-bernoulli-categorical: Done
-5. encoder-categorical-bernoulli: medium
+5. encoder-categorical-bernoulli: same as decoder-categorical bernoulli?
 6. encoder-universe: hard
 
-7. decoder-and: medium
+7. decoder-and: done (to review)
 8. decoder-attention: Done (was already good)
 9. decoder-token: Done (was already good)
-10. decoder-bernoulli-categorical: medium
-11. decoder-categorical-bernoulli: hard
+10. decoder-bernoulli-categorical: same as encoder-bernoulli-categorical?
+11. decoder-categorical-bernoulli: done (to review)
 12. decoder-universe: hard
 
 
