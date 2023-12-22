a23dd26354c9ca676508e4e986e971e980684ae0
ThomasRochais <thb@sas.upenn.edu>
2023-12-22 09:13:32 -0500

    feat: merged in encoder_position_pi from the inductive_transformer_position repo

--- encoder_layer.py
+++ encoder_layer.py
@@ -2,6 +2,7 @@ from torch import nn  # type: ignore
 from encoder_universe import EncoderUniverse
 from encoder_bernoulli_categorical import EncoderBernoulliCategorical
 from encoder_token_pi import EncoderTokenPi
+from encoder_position_pi import EncoderPositionPi
 from encoder_attention_pi import EncoderAttentionPi
 from encoder_categorical_bernoulli import EncoderCategoricalBernoulli
 from encoder_and import EncoderAnd
@@ -15,13 +16,14 @@ class EncoderLayer(nn.Module):
         self.encoder_universe = EncoderUniverse(hyperparams=hyperparams, active_layer=active_layer)
         self.encoder_bernoulli_categorical = EncoderBernoulliCategorical(hyperparams=hyperparams, active_layer=active_layer)
         self.encoder_token_pi = EncoderTokenPi(hyperparams=hyperparams, active_layer=active_layer)
+        self.encoder_position_pi = EncoderPositionPi(hyperparams=hyperparams, active_layer=active_layer)
         self.encoder_attention_pi = EncoderAttentionPi(hyperparams=hyperparams, active_layer=active_layer)
         self.encoder_categorical_bernoulli = EncoderCategoricalBernoulli(hyperparams=hyperparams, active_layer=active_layer)
         self.encoder_and = EncoderAnd(hyperparams=hyperparams, active_layer=active_layer)
 
         self.z_prime = None
 
-    def forward(self, z, t):
+    def forward(self, z, t_categorical):
         # dim=0 indexes the state of the variable e.g. cat or dog, 0 or 1, etc.
         # dim=1 indexes the layer width
 
@@ -34,9 +36,25 @@ class EncoderLayer(nn.Module):
 
         # Encoder Attention $\pi$
         y_categorical = self.encoder_attention_pi(v)
+        assert y_categorical.shape == (1, self.hyperparams.layer_width)
 
         # Encoder Word $\pi$
-        x_categorical = self.encoder_token_pi(t)
+        # without position, we had
+        # t_categorical.shape() = (vocab_size, layer_width)
+        # now with position we have:
+        assert t_categorical.shape == (self.hyperparams.num_positions, self.hyperparams.vocab_size, self.hyperparams.layer_width)
+
+        # TODO:
+        # The encoder open-closed universe without backwards info from the decoder simply clones the input data for a given token and position
+        # and sends it to the corresponding pi_t in both the left and right columns.
+
+        # We simply make the input data have the same values for every value of lw (which indexes layer_width).
+        rho_categorical = self.encoder_token_pi(t_categorical)
+        assert rho_categorical.shape == (self.hyperparams.num_positions, self.hyperparams.layer_width)
+
+        # Encoder Word $\pi$
+        x_categorical = self.encoder_position_pi(rho_categorical)
+        assert x_categorical.shape == (1, self.hyperparams.layer_width)
 
         # Encoder Categorical-Bernoulli
         y_bernoulli = self.encoder_categorical_bernoulli(y_categorical)
