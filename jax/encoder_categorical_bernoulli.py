b9b6e886bc14cd59ec8c33a4c911974faf3c8b97
Ben Vigoda <ben@benvigoda.com>
2024-03-04 09:00:14 -0500

    jax/encoder_categorical_bernoulli.py ready for review

---
+++ jax/encoder_categorical_bernoulli.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+from flax import linen as nn
+from typing import Callable
+import jax.numpy as jnp
+
+from helper_functions import custom_normalize
+
+@dataclass
+class EncoderCategoricalBernoulli(nn.Module):
+    layer_width: int
+
+
+    def __call__(self, categorical):
+
+        # categorical is size = (1, layer_width)
+        assert categorical.shape == (1, self.layer_width)
+        # bernoulli is size (2, layer_width)
+
+        # The probability of a bernoulli variable being true is the same as the probability of the
+        # corresponding categorical state.
+
+        bernoulli_1 = categorical
+
+        # The probability of a bernoulli variable being false is the sum of the probabilities of all
+        # the other categorical states.
+        # Note: if categorical[i][j] is much larger than categorical[i][k] for k != j, then this
+        # method of performing the calculation introduces a lot of rounding error.
+
+        bernoulli_0 = categorical.sum(axis=-1, keepdim=True) - categorical
+
+        bernoulli = jnp.stack([bernoulli_0, bernoulli_1])
+
+        bernoulli = custom_normalize(bernoulli, axis=0)
+
+        return bernoulli
