79179a45a2b229bb7f8a24bc109918dd214615ff
Erik Strand <erik.strand@cba.mit.edu>
2024-03-15 16:55:32 -0400

    Zero relevant gradients (encoder position pi)

--- jax/weights.py
+++ jax/weights.py
@@ -1,4 +1,6 @@
+import jax
 import jax.numpy as jnp  # type: ignore
+import numpy as np
 
 STRONG = 1.  # Amplify the signal
 WEAK = 1e-9  # Dampen the signal
@@ -12,19 +14,25 @@ def update_weights(params):
 
     """Update weights."""
     updated_params = params
+    mask_type = int
+    set_weights = jax.tree_util.tree_map(lambda x: jnp.ones_like(x, dtype=mask_type), params)
     num_layer = 0
     done = False
     while not done:
         encoder_layer_key = f"encoders_{num_layer}"
         if encoder_layer_key in updated_params["params"]:
             # Get the shape of the original weights
-            weights_shape = updated_params["params"][encoder_layer_key]["encoder_position_pi"]["weights"].shape  # (num_positions, layer_width)
+            old_weights = updated_params["params"][encoder_layer_key]["encoder_position_pi"]["weights"]
+            weights_shape = old_weights.shape  # (num_positions, layer_width)
             # Set the weights to all "weak" values
             new_weight = jnp.full(weights_shape, weak)
             new_weight = new_weight.at[num_layer].set(jnp.full(layer_width, strong))  # Match num_layer to the position in the weights
             updated_params["params"][encoder_layer_key]["encoder_position_pi"]["weights"] = new_weight
             # Note: We have constrained the model such that num_positions needs to be equal to num_layers for this setup to work right now.
             # In the future we'll have to remove that constraint
+
+            set_weights["params"][encoder_layer_key]["encoder_position_pi"]["weights"] = jnp.zeros_like(old_weights, dtype=mask_type)
+
             num_layer += 1
         else:
             done = True
@@ -36,5 +44,4 @@ def update_weights(params):
     new_weight = new_weight.at[position, vocab_idx, lw].set(strong)
     updated_params["params"]["encoders_0"]["encoder_token_pi"]["weights"] = new_weight
 
-    set_weights = updated_params  # TODO @Erik: What's the best way to pass in the set of weights that should be kept as gradient 0?
     return updated_params, set_weights
