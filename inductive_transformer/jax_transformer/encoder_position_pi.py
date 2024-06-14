90dafea8a6bda9a8bbf34200091c49fa92385364
Erik Strand <erik.strand@cba.mit.edu>
2024-06-14 17:06:28 -0400

    Don't normalize the output of encoder position pi

--- inductive_transformer/jax_transformer/encoder_position_pi.py
+++ inductive_transformer/jax_transformer/encoder_position_pi.py
@@ -48,5 +48,5 @@ class EncoderPositionPi(nn.Module):
 
         # make it an inner product by taking a sum along the token dimension
         x = jnp.sum(x, axis=0, keepdims=True)  # after summing it is size = (1, layer_width)
-        x = custom_normalize(x, axis=1)
+        # x = custom_normalize(x, axis=1)
         return x  # x is categorical
