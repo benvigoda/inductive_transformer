fixing normalization and other knit picks that matter in the decoder.  doing this by tracing activations through the network for a simple example of inputs and weights

inductive_transformer/jax_transformer/decoder_universe.py
@@ -11,7 +11,7 @@ class DecoderUniverse:
     def __call__(self, u):
         assert u.shape == (2, self.layer_width, self.layer_width)
 
-        z = jnp.sum(u, axis=1)
+
         """
         # u[heads/tails][below_lw?][above_lw?]
         # left
@@ -24,5 +24,18 @@ class DecoderUniverse:
 
         # z = nn.functional.normalize(z, p=1, dim=0)
         """
-        z = custom_normalize(z, axis=0)
+        # not sure why we had a sum?  it was some sort of approximation?
+        # z = jnp.sum(u, axis=1)
+       
+        # z[0][0] = u[0][0][0] * u[0][0][1]
+        # z[1][0] = 1 - z[0][0]
+        # z[0][1] = u[0][1][0] * u[0][1][1]
+        # z[1][1] = 1 - z[0][1]
+        # more general form of this:
+        z_0 = jnp.prod(u[0], axis=-1)
+        z_1 = 1 - z_0
+        z = jnp.stack([z_0, z_1], axis=0)
+
+        # z = custom_normalize(z, axis=0)  # Has to be unecessary Otherwise we would not be allowed to do z_1 = 1 - z_0
+        assert z.shape == (2, self.layer_width)
         return z