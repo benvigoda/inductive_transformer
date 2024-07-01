fixing normalization and other knit picks that matter in the decoder.  doing this by tracing activations through the network for a simple example of inputs and weights

inductive_transformer/jax_transformer/decoder_attention_pi.py
@@ -26,9 +26,12 @@ class DecoderAttentionPi(nn.Module):
         prob_weights = custom_normalize(prob_weights, axis=1)
 
         # and then since y comes in as categorical of size (1, layer_width)
-        y = custom_normalize(y, axis=1)
+        # y = custom_normalize(y, axis=1)
 
         # element-wise product of weight tensor and y
         v = prob_weights * y
+
+        # added this:
+        v = custom_normalize(v, axis=1)
         assert v.shape == (self.layer_width, self.layer_width)
         return v

inductive_transformer/jax_transformer/decoder_categorical_bernoulli.py
@@ -59,7 +59,8 @@ class DecoderCategoricalBernoulli:
         # the other categorical states.
         # Note: if v[i][j] is much larger than v[i][k] for k != j, then this method of performing
         # the calculation introduces a lot of rounding error.
-        u_0 = jnp.sum(v, axis=-1, keepdims=True) - v
+        # u_0 = jnp.sum(v, axis=-1, keepdims=True) - v
+        u_0 = 1 - u_1
 
         u = jnp.stack([u_0, u_1], axis=0)
 