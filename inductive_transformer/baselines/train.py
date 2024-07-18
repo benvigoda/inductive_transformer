6dbf340d1d111d619bdbeaa4219f0ebf1e6eaa83
Erik Strand <erik.strand@cba.mit.edu>
2024-07-18 14:47:29 -0700

    Sample

--- inductive_transformer/baselines/train.py
+++ inductive_transformer/baselines/train.py
@@ -46,10 +46,9 @@ def generate_batch(key, data, vocab_size, batch_size):
     key, subkey = jax.random.split(key)
     data = sample_sentences(subkey, data, batch_size)
     assert data.shape == (batch_size, sentence_length)
-    p = 1.0 / sentence_length
+    p = 1.0 / (sentence_length + 1)
     masked_data, mask = blank_out_words(key, data, p, vocab_size)
-    masked_data = word_ids_to_one_hot(masked_data, vocab_size)
-    assert masked_data.shape == (batch_size, sentence_length, vocab_size)
+    assert masked_data.shape == (batch_size, sentence_length)
     return masked_data, data, mask
 
 
@@ -73,6 +72,7 @@ def train_step(state, batch_x, batch_y):
 def full_train_step(key, data, state, vocab_size, batch_size):
     step_key = jax.random.fold_in(key, state.step)
     batch_x, batch_y, _ = generate_batch(step_key, data, vocab_size, batch_size)
+    batch_x = word_ids_to_one_hot(batch_x, vocab_size)
     return train_step(state, batch_x, batch_y)
 
 
@@ -103,13 +103,12 @@ def main(dataset):
     print("")
 
     print("Initializing model...")
-    learning_rate = 1e-4
+    learning_rate = 2e-5
     sentence_words = data.sentence_length * data.vocab_size
     layers = [
         4 * sentence_words,
-        3 * sentence_words,
-        2 * sentence_words,
         sentence_words,
+        4 * sentence_words,
     ]
     key, subkey = jax.random.split(key)
     model, train_state = make_train_state(
@@ -119,9 +118,31 @@ def main(dataset):
 
     print("Training...")
     batch_size = 256
-    n_steps = 20000
+    n_steps = 10000
     key, subkey = jax.random.split(key)
     state = train(subkey, data, train_state, batch_size, n_steps)
+    print("")
+
+    print("Sampling...")
+    n_samples = 50
+    key, subkey = jax.random.split(key)
+    sample_x_ids, sample_y_ids, mask = generate_batch(subkey, data.data, data.vocab_size, n_samples)
+    sample_x_words = data.ids_to_strings(sample_x_ids)
+    sample_y_words = data.ids_to_strings(sample_y_ids)
+    sample_x_one_hot = word_ids_to_one_hot(sample_x_ids, data.vocab_size)
+    log_probs = state.apply_fn(state.params, sample_x_one_hot)
+    key, subkey = jax.random.split(key)
+
+    # This samples according to the categorical distribution given by log_probs.
+    # generated_ids = jax.random.categorical(subkey, log_probs)
+
+    # This chooses the most likely word.
+    probs = jax.nn.softmax(log_probs)
+    generated_ids = jnp.argmax(probs, axis=-1)
+
+    generated_words = data.ids_to_strings(generated_ids)
+    for id in range(n_samples):
+        print(sample_y_words[id], "=>", sample_x_words[id], "=>", generated_words[id])
 
 
 if __name__ == "__main__":
