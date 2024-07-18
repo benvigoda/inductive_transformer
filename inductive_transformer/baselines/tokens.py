df043ba5d488f52c2f88a16cac8373ebb96fc693
Erik Strand <erik.strand@cba.mit.edu>
2024-07-18 14:10:59 -0700

    Start training

--- inductive_transformer/baselines/tokens.py
+++ inductive_transformer/baselines/tokens.py
@@ -1,14 +1,40 @@
-import numpy as np
+import jax
+import jax.numpy as jnp
+from typing import NamedTuple
+
+
+class Dataset(NamedTuple):
+    word_to_id: dict[str, int]
+    id_to_word: dict[int, str]
+    data: jax.Array  # axes are sentence, word position (in sentence), word id
+
+    @property
+    def n_sentences(self):
+        return self.data.shape[0]
+
+    @property
+    def sentence_length(self):
+        return self.data.shape[1]
+
+    @property
+    def vocab_size(self):
+        return len(self.word_to_id)
+
+    @property
+    def blank_token(self):
+        return self.vocab_size - 1
 
 
 def load_dataset(filepath):
-    with open(filepath, 'r') as f:
+    # Load the file into memory.
+    with open(filepath, "r") as f:
         data = f.read()
 
     # Split on '.' and drop empty sentences.
-    sentences = [x.strip() for x in data.strip().split('.')]
-    sentences = [x for x in sentences if x != '']
-    assert len(sentences) > 0
+    sentences = [x.strip() for x in data.strip().split(".")]
+    sentences = [x for x in sentences if x != ""]
+    n_sentences = len(sentences)
+    assert n_sentences > 0
 
     # Split on ' '.
     words = [x.split() for x in sentences]
@@ -16,6 +42,7 @@ def load_dataset(filepath):
     # Check that all sentences have the same length.
     lengths = set([len(x) for x in words])
     assert len(lengths) == 1
+    sentence_length = list(lengths)[0]
 
     # Create a vocabulary.
     word_to_id = {}
@@ -25,13 +52,16 @@ def load_dataset(filepath):
                 word_to_id[word] = len(word_to_id)
     word_to_id["BLANK"] = len(word_to_id)
     id_to_word = {v: k for k, v in word_to_id.items()}
-    vocab_size = len(word_to_id)
 
-    print("Vocabulary")
-    for id in range(vocab_size):
-        print(id, id_to_word[id])
-    print("")
+    # print("Vocabulary")
+    # vocab_size = len(word_to_id)
+    # for id in range(vocab_size):
+    #     print(id, id_to_word[id])
+    # print("")
 
+    # Convert words to ids.
+    word_ids = [[word_to_id[word] for word in sentence] for sentence in words]
+    word_ids = jnp.array(word_ids)
+    assert word_ids.shape == (n_sentences, sentence_length)
 
-if __name__ == "__main__":
-    load_dataset("../../32_2_layer_sentences.txt")
+    return Dataset(word_to_id, id_to_word, word_ids)
