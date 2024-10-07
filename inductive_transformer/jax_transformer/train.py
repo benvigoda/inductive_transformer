62696ce7bce4faa28edf6950a87b1478198f6d01
ThomasRochais <thb@sas.upenn.edu>
2024-10-07 07:55:32 -0400

    wip

--- inductive_transformer/jax_transformer/train.py
+++ inductive_transformer/jax_transformer/train.py
@@ -478,7 +478,7 @@ def main():
             state = update_model(state, grads)
 
             # if (epoch % print_every == 0 or epoch == n_epochs - 1) and step_idx == 0:
-            if (epoch % print_every == 0 or epoch == n_epochs - 1) and step_idx % 10 == 0 and step_idx < 500:
+            if (epoch % print_every == 0 or epoch == n_epochs - 1) and step_idx % 50 == 0 and step_idx < 500:
                 print("\nTop:", "↓" * 100)
                 print(f"epoch {epoch}, loss: {loss:.20e}")
                 printed_weights = print_params(state, data.vocab, silence_print=True)
