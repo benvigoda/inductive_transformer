fixed encoder log_weights normalized, but the we need to think through the log_softmax normalization of the decoder, especially in terms of along what axis we should normalize and whether or not we should even normalize in the first place (as in decoder token pi for instance)

jax_transformer/decoder_categorical_bernoulli.py
@@ -14,8 +14,9 @@ class DecoderCategoricalBernoulli:
         u_1 = v
 
         # The probability of a bernoulli variable being False is 1 - the probability of it being True.
-        u_0 = 1.0 - u_1
-        u_0 = nn.log_sigmoid(-u_1)
+        # u_0 = 1.0 - u_1
+        u_0 = jnp.log1p(-u_1)
+        # u_0 = nn.log_sigmoid(-u_1)
 
         u = jnp.stack([u_0, u_1], axis=0)
 

jax_transformer/decoder_position_pi.py
@@ -33,8 +33,8 @@ class DecoderPositionPi(nn.Module):
         # element-wise product of weight tensor and y
         rho = prob_weights * x
         '''
-        
+
         rho = log_weights + x
-        
+
         assert rho.shape == (self.num_positions, self.layer_width)
         return rho

jax_transformer/encoder_attention_pi.py
@@ -22,7 +22,7 @@ class EncoderAttentionPi(nn.Module):
         weights = self.param(
             "weights", self.weight_init, (self.layer_width, self.layer_width)
         )
-        log_weights = log_softmax(weights)
+        log_weights = log_softmax(weights, axis=0)
         # prob_weights = nn.relu(weights) + EPSILON
         # prob_weights = custom_normalize(prob_weights, axis=1)
         # # in the future we may want to normalize v here for good measure

jax_transformer/encoder_position_pi.py
@@ -21,7 +21,7 @@ class EncoderPositionPi(nn.Module):
         weights = self.param(
             "weights", self.weight_init, (self.num_positions, self.layer_width)
         )
-        log_weights = log_softmax(weights)
+        log_weights = log_softmax(weights, axis=0)
 
         # prob_weights = nn.relu(weights) + EPSILON
         # # NOTE: we decided to normalize the weights (it shouldn't matter)

jax_transformer/weights_broad_init.py
@@ -1,11 +1,14 @@
 import jax  # type: ignore
 import jax.numpy as jnp  # type: ignore
 import numpy as np  # type: ignore
-from jax_transformer.helper_functions import EPSILON, get_num_layers
+from jax_transformer.helper_functions import EPSILON, get_num_layers, PROBABLE, IMPROBABLE
 from inductive_transformer.datasets.anavan import make_cat_dog_anavan, make_cat_dog_worm_bird_anavan  # type: ignore
 
-strong = 1.0 - EPSILON  # Amplify the signal
-weak = EPSILON  # Dampen the signal
+# strong = 1.0 - EPSILON  # Amplify the signal
+# weak = EPSILON  # Dampen the signal
+
+strong = 0 - EPSILON
+weak = jnp.log(EPSILON)
 
 mask_type = int
 
@@ -55,8 +58,6 @@ def set_position_pi_weights(
         raise ValueError(f"Layer {layer_key} not found in params.")
 
 
-
-
 def init_weights(
     params,
     vocab,
@@ -66,8 +67,6 @@ def init_weights(
     perturb_token=None,
     perturb_attention=None,
     surgical_perturb=False,
-    zero_out_right_weights=False,
-    zero_out_left_weights=False,
     noise_value=0.01,
     catsanddogs=False,
 ):
@@ -75,10 +74,6 @@ def init_weights(
         synonyms = make_cat_dog_anavan()
     else:
         synonyms = make_cat_dog_worm_bird_anavan()
-    if zero_out_right_weights:
-        synonyms.zero_right_words()
-    if zero_out_left_weights:
-        synonyms.zero_left_words()
     # Get shapes:
     num_positions, vocab_size, layer_width = params["params"]["encoders_0"][
         "encoder_token_pi"
@@ -287,7 +282,6 @@ def init_weights(
             #     else:
             #         new_weights = new_weights.at[[0][0]].set(new_weights[0][0] - jax.random.normal(jax.random.PRNGKey(np.random.default_rng().integers(0, 2**32 - 1))) * noise_value)
 
-
             if perturb_weights or perturb_attention:
                 if not surgical_perturb:
                     # Add a small amount of noise to the weights
@@ -297,7 +291,7 @@ def init_weights(
             updated_params["params"][f"{encoders_decoders}_{layer}"][
                 f"{encoder_decoder}_attention_pi"
             ]["weights"] = new_weight
-            
+
             # Fix set_weights so the gradient does not update the weights
             if lock_all_weights:
                 set_weights["params"][f"{encoders_decoders}_{layer}"][